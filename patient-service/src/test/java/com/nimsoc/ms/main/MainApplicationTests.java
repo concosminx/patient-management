eb8824421db7dee47889106d8e0a724a6ccc2791
Cosmin C. <concosminx@gmail.com>
2025-09-20 15:54:33 +0300

    initial commit

---
+++ patient-service/src/test/java/com/nimsoc/ms/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimsoc.ms.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
